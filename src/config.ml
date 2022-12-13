c441f8b1a6cdc234098d8660617e123488b13033
Benjamin Loison <benjamin.loison@orange.fr>
2022-12-13 16:03:05 +0100

    Correct typo in comment in `src/config.ml`

--- src/config.ml
+++ src/config.ml
@@ -1,4 +1,4 @@
-(** Maximum Number of variabnles declared in a sngle node.
+(** Maximum Number of variables declared in a single node.
     * This corresponds to the sum of the number of local, input and output
     * variables. *)
 let maxvar = 100
