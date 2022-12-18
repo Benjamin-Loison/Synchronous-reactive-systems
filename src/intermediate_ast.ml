77c865e36084672c2421e007e7c4b821e9f549c8
Arnaud DABY-SEESARAM <ds-ac@crans.org>
2022-12-18 17:25:34 +0100

    [intermediate_ast] remove unused fields of i_nodes

--- src/intermediate_ast.ml
+++ src/intermediate_ast.ml
@@ -29,9 +29,6 @@ type node_state =
     nt_nb_int : int;
     nt_nb_real: int;
     nt_nb_bool: int;
-    nt_map_int: (ident * bool, int) Hashtbl.t;
-    nt_map_bool: (ident * bool, int) Hashtbl.t;
-    nt_map_real: (ident * bool, int) Hashtbl.t;
     nt_map: (ident * bool, string * int) Hashtbl.t;
     nt_output_map: (int, string * int) Hashtbl.t;
     nt_prevars: t_var list;
